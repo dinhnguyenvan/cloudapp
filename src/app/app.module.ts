1dde7eaafb51d5e57c6263f563c30ee7da9c90e3
dinhnvk3@gmail.com <dinhnvk3@gmail.com>
2018-03-28 00:02:50 +0700

    te

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgR_Mr_0eUo-wRtP-Ni-vTYTMDszFAAV4",
+  authDomain: "dinhnvk3-d2aac.firebaseapp.com",
+  databaseURL: "https://dinhnvk3-d2aac.firebaseio.com",
+  projectId: "dinhnvk3-d2aac",
+  storageBucket: "dinhnvk3-d2aac.appspot.com",
+  messagingSenderId: "440945231168"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  songs: AngularFireList<any>;
+  constructor(public navCtrl: NavController, afDatabase: AngularFireDatabase) {
+    this.songs = afDatabase.list('/songs').valueChanges();
+  }
+
+}
